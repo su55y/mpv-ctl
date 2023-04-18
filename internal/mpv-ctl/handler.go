db0a46c94ccc34b3ce663f0696d388876dd1ed11
su55y <cesarmilano88@yahoo.com>
2023-04-18 20:21:19 +0300

    unified params

--- internal/mpv-ctl/handler.go
+++ internal/mpv-ctl/handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/blang/mpv"
 )
@@ -48,21 +49,34 @@ func middleHandler() func(http.Handler) http.Handler {
 	}
 }
 
-func (h *handler) appendHandler(w http.ResponseWriter, r *http.Request) {
-	if !r.URL.Query().Has("path") {
-		writeError(&w, "'path' param should be present")
-		return
+type ParamsType1 struct {
+	url  string
+	flag string
+}
+
+func parseParamsType1(query url.Values) (*ParamsType1, error) {
+	if !query.Has("url") {
+		return nil, fmt.Errorf("'url' param should be present")
 	}
 	flag := mpv.LoadFileModeReplace
-	if r.URL.Query().Has("flag") {
-		switch r.URL.Query().Get("flag") {
+	if query.Has("flag") {
+		switch query.Get("flag") {
 		case mpv.LoadFileModeAppend:
 			flag = mpv.LoadFileModeAppend
 		case mpv.LoadFileModeAppendPlay:
 			flag = mpv.LoadFileModeAppendPlay
 		}
 	}
-	if err := h.mpvc.Loadfile(r.URL.Query().Get("path"), flag); err != nil {
+	return &ParamsType1{query.Get("url"), flag}, nil
+}
+
+func (h *handler) appendHandler(w http.ResponseWriter, r *http.Request) {
+	params, err := parseParamsType1(r.URL.Query())
+	if err != nil {
+		writeError(&w, err.Error())
+		return
+	}
+	if err := h.mpvc.Loadfile(params.url, params.flag); err != nil {
 		errorMsg := fmt.Sprintf("append error: %s", err.Error())
 		log.Println(errorMsg)
 		writeError(&w, errorMsg)
@@ -72,20 +86,12 @@ func (h *handler) appendHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *handler) loadPlaylistHandler(w http.ResponseWriter, r *http.Request) {
-	if !r.URL.Query().Has("path") {
-		writeError(&w, "'path' param should be present")
+	params, err := parseParamsType1(r.URL.Query())
+	if err != nil {
+		writeError(&w, err.Error())
 		return
 	}
-	flag := mpv.LoadFileModeReplace
-	if r.URL.Query().Has("flag") {
-		switch r.URL.Query().Get("flag") {
-		case mpv.LoadFileModeAppend:
-			flag = mpv.LoadFileModeAppend
-		case mpv.LoadFileModeAppendPlay:
-			flag = mpv.LoadFileModeAppendPlay
-		}
-	}
-	if err := h.mpvc.LoadList(r.URL.Query().Get("path"), flag); err != nil {
+	if err := h.mpvc.LoadList(params.url, params.flag); err != nil {
 		errorMsg := fmt.Sprintf("load playlist error: %s", err.Error())
 		log.Println(errorMsg)
 		writeError(&w, errorMsg)
